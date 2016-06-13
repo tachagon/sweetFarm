13c73da04ddcee3e8ad9ae5b28a70db727641d7e
Tatchagon <o_k_t@hotmail.com>
2016-06-13 16:04:06 +0700

    Coundown timer

--- app/assets/javascripts/customs.js
+++ app/assets/javascripts/customs.js
@@ -199,3 +199,44 @@ var show_rating = function (target_id, rating_avg) {
 
   document.getElementById(target_id).innerHTML = output;
 }
+
+
+var getTimeRemaining = function (endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+var initializeClock = function (id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+      clock.innerHTML = "<p class='center' style='color: red;'>หมดอายุแล้ว</p>";
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
