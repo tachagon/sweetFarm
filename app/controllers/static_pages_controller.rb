6931596c77edff52f11a65f318dc43982754dd74
Tatchagon <o_k_t@hotmail.com>
2016-06-16 16:48:42 +0700

    new index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,16 @@
 class StaticPagesController < ApplicationController
 
 	def index
-    @per_page = params[:per_page] ? params[:per_page] : 12
-    @announcements = Announcement.show.not_expired.recent.paginate(page: params[:page], per_page: @per_page)
-    respond_to do |format|
-      format.html
-      format.json{render json: @announcements}
-      format.xml{render xml: @announcements}
+    if logged_in?
+      @per_page = params[:per_page] ? params[:per_page] : 12
+      @announcements = Announcement.show.not_expired.recent.paginate(page: params[:page], per_page: @per_page)
+      respond_to do |format|
+        format.html
+        format.json{render json: @announcements}
+        format.xml{render xml: @announcements}
+      end
     end
+
 	end
 
 	def page2
--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -8,4 +8,63 @@ module AnnouncementsHelper
     end
   end
 
+  def get_deal_status(announcement)
+    deals = announcement.deals
+    deals.each do |deal|
+      return "ซื้อขายสำเร็จแล้ว" if completed?(deal)
+      return "ได้รับรีวิวแล้ว" if reviewed?(deal)
+      return "ขนส่งแล้ว" if shipped?(deal)
+      return "ชำระเงินแล้ว" if paid?(deal)
+      return "รับข้อเสนอแล้ว" if accepted?(deal)
+    end
+  end
+
+  def accept_announcement?(announcement)
+    deals = announcement.deals
+    deals.each do |deal|
+      return true if accepted?(deal) || paid?(deal) || shipped?(deal) || reviewed?(deal) || completed?(deal)
+    end
+    return false
+  end
+
+  def accepted?(deal)
+    if deal.status == "accepted"
+      true
+    else
+      false
+    end
+  end
+
+  def paid?(deal)
+    if deal.status == "paid"
+      true
+    else
+      false
+    end
+  end
+
+  def shipped?(deal)
+    if deal.status == "shipped"
+      true
+    else
+      false
+    end
+  end
+
+  def reviewed?(deal)
+    if deal.status == "reviewed"
+      true
+    else
+      false
+    end
+  end
+
+  def completed?(deal)
+    if deal.status == "completed"
+      true
+    else
+      false
+    end
+  end
+
 end
